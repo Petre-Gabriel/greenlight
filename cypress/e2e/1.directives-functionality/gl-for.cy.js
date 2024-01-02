17403a1d933e9f56521f48597ec3ff2fe7395f01
Petre Gabriel <gabrielpetre04@gmail.com>
2024-01-02 13:01:40 +0200

    Imported removeGLElement for cleanup phase of the gl-for

--- cypress/e2e/1.directives-functionality/gl-for.cy.js
+++ cypress/e2e/1.directives-functionality/gl-for.cy.js
@@ -16,4 +16,22 @@ describe("gl-for tests", () => {
       });
     });
   });
+
+  it("Should remove on length 0", () => {
+    cy.visit("http://127.0.0.1:5500/index.html");
+
+    cy.window().then((win) => {
+      const Controller = win.GreenLight.controller("LoginForm");
+
+      // It starts with a length of 3
+      cy.get("ul > li").should("have.length", 3);
+
+      // We wait because it updates faster than cypress processes it.
+      cy.wait(100).then(() => {
+        Controller.$store.get().names = [];
+
+        cy.get("ul > li").should("have.length", 0);
+      });
+    });
+  });
 });
