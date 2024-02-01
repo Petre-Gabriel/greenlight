59bcc9b71c0d7873900233bc4005936653cdc686
Petre Gabriel <gabrielpetre04@gmail.com>
2024-02-01 13:14:16 +0200

    feat: added async evaluator for directives, changed the way to access global store in directives and updated tests.

--- cypress/e2e/1.directives-functionality/gl-if.cy.js
+++ cypress/e2e/1.directives-functionality/gl-if.cy.js
@@ -1,18 +1,18 @@
 describe("gl-if tests", () => {
-  it("should show the div after updating username input", () => {
+  it("should show the div after updating username input with 5 characters", () => {
     cy.visit("http://127.0.0.1:5500/index.html");
 
-    cy.get("#username").type("test");
+    cy.get("#username").type("123456");
 
-    cy.get("[gl-if=user\\.name]").each((el) => {
+    cy.get(".gl-if-test").each((el) => {
       cy.wrap(el).should("be.visible");
     });
   });
 
-  it("should not show the div if the username is empty", () => {
+  it("should not show the div if the username has less than 5 characters", () => {
     cy.visit("http://127.0.0.1:5500/index.html");
 
-    cy.get("[gl-if=user\\.name]").each((el) => {
+    cy.get(".gl-if-test").each((el) => {
       cy.wrap(el).should("not.be.visible");
     });
   });
